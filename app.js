618a89035ea168d5351001fae478065bbcf8c323
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-21 19:26:51 +0100

    1.0 /

--- app.js
+++ app.js
@@ -12,27 +12,23 @@ let compteur = document.getElementById('interval-div');
 let variableChiffre = 0;
 let variableSoixante = 0;
 
-function time () {
-    if (variableChiffre < 60) {
-        compteur.innerHTML = variableChiffre;
-        variableChiffre++;
-    } else if (variableChiffre === 60) {
-        variableChiffre = 0;
-        variableSoixante++;
-        document.getElementById('divMoche').innerHTML = variableSoixante;
-    }
+function tiniTime (){
+    let timeOut = setInterval(function (){
+        if (variableChiffre < 60) {
+            compteur.innerHTML = variableChiffre;
+            variableChiffre++;
+        } else if (variableChiffre === 60) {
+            variableChiffre = 0;
+            variableSoixante++;
+            document.getElementById('divMoche').innerHTML = variableSoixante;
+        }
+    }, 1000);
+
+    document.getElementById('interval-stop').addEventListener('click', function (){
+        clearInterval(timeOut);
+    })
+
 }
 
 //Start:
-document.getElementById('interval-start').addEventListener('click', function(){
-    setInterval(time ,1000);
-});
-
-//Stop
-document.getElementById('interval-stop').addEventListener('click', function (){
-    clearTimeout(time);
-})
-
-//    S'arrêter lors du clic sur le bouton #interval-stop.
-//Incrémenter une variable de 60 ( toutes les 60 secondes du coups )
-//A chaque fois mettre à jour le contenu du div #interval-div avec la nouvelle valeur de la variable incrémentée.
+document.getElementById('interval-start').addEventListener('click', tiniTime);
